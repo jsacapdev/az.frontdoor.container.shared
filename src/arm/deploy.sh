002ce21f9def8dd3cd3f2be5243d905ee9c90949
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2023-03-31 14:16:16 +0100

    fade8faa-713a-4b04-aef1-da74a0163845

---
+++ src/arm/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+echo "Resource Group: $1";
+echo "Deployment Name: $2";
+echo "Location: $3";
+
+RESOURCE_GROUP_NAME=$1
+DEPLOYMENT_NAME=$2
+LOCATION=$3
+
+# RESULT=$(az deployment group create \
+#     --resource-group $RESOURCE_GROUP_NAME \
+#     --name $DEPLOYMENT_NAME \
+#     --template-file ./main.bicep \
+#     --parameters baseName=$BASE_NAME \
+#     --query properties.outputs.result)
+
+echo Finshed.
