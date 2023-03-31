4d724538f1b0a02b0f148b2ce7bd4a003dadb2d1
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2023-03-31 14:50:45 +0100

    72eb1746-02de-4f9e-805b-5aba4d8a08d0

--- src/arm/deploy.sh
+++ src/arm/deploy.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Resource Group: $1";
-echo "Deployment Name: $2";
-echo "Location: $3";
-echo "Workspace: $4";
+echo "Parameters..."
+echo "Resource Group -> $1";
+echo "Deployment Name -> $2";
+echo "Location -> $3";
+echo "Workspace -> $4";
 
 RESOURCE_GROUP_NAME=$1
 DEPLOYMENT_NAME=$2
 LOCATION=$3
 WORKSPACE=$4
-
-# workaround to get bicep to work with azure cli github action
-az config set bicep.use_binary_from_path=false
-az bicep install
-
-ls -al
 
 RESULT=$(az deployment group create \
     --resource-group $RESOURCE_GROUP_NAME \
@@ -23,7 +18,5 @@ RESULT=$(az deployment group create \
     --template-file $WORKSPACE/main.bicep \
     --parameters location=$LOCATION \
     --query properties.outputs.result)
-
-# exit 1
 
 echo Finshed.
